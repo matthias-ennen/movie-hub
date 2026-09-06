e2a18baae77f9e3d8f5fbd5721ed80d94f0ca409
matthias-ennen <matthias.ennen@gmx.de>
2026-09-06 20:57:12 +0200

    Phase 2.3: Provider-Hierarchie an Auth koppeln

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { ThemeProvider } from './theme/ThemeProvider.jsx'
 import './index.css'
 import './styles/themes.css'
 import './styles/profile.css'
@@ -10,8 +9,6 @@ import './styles/tmdb.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <App />
   </StrictMode>,
 )
