0ab33970d095d9859111ba2e89dedb9576520bc1
matthias-ennen <matthias.ennen@gmx.de>
2026-09-06 20:27:21 +0200

    Load TMDB artwork styles

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import './index.css'
 import './styles/themes.css'
 import './styles/profile.css'
 import './styles/tv.css'
+import './styles/tmdb.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
