31b96e8234c532e6549c0484a7a9daafd3ac64c8
matthias-ennen <matthias.ennen@gmx.de>
2026-09-17 10:30:23 +0200

    #218: Layout-Korrekturen zuletzt laden

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import InitialHomeFocus from './components/InitialHomeFocus.jsx'
 import './index.css'
 import './styles/themes.css'
 import './styles/profile.css'
@@ -15,9 +16,11 @@ import './styles/issue128-row-alignment.css'
 import './styles/top-ten.css'
 import './styles/film-collections.css'
 import './styles/series-navigation.css'
+import './styles/issue218.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <InitialHomeFocus />
     <App />
   </StrictMode>,
 )
